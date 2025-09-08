c3348a900fd9fc64428b5cc80c1f1d051ea6b58d
lunwes <lunwes>
2025-09-08 20:32:39 -0300

    métodos construtores

---
+++ POO_252_noite/Construtores/ConstrutorConta/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConstrutorConta;
+
+Conta c1 = new Conta();
+c1.MostrarAtributos();
+
+Conta c2 = new Conta(2, "Bia");
+c2.MostrarAtributos();
+
+Conta c3 = new Conta(3);
+c3.MostrarAtributos();
---
+++ POO_252_noite/Construtores/ConstrutorFuncionario/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConstrutorFuncionario;
+
+Funcionario f1 = new Funcionario();
+f1.MostrarAtributos();
+
+Funcionario f2 = new Funcionario(2);
+f2.MostrarAtributos();
+
+Funcionario f3 = new Funcionario(3, "Paula");
+f3.MostrarAtributos();
+
+Funcionario f4 = new Funcionario(4, "Jorgina", 3000.50);
+f4.MostrarAtributos();
